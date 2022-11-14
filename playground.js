db25fab237103bd38eb7958fe805d9cd892cc2e1
cmendez20 <cmendez20@utexas.edu>
2022-11-14 00:39:17 -0600

    Add practice exercise notes

---
+++ playground.js
@@ -0,0 +1,20 @@
+// for (let i = 10; i >= 0; i -= 1) {
+//   i === 0 ? console.log('Launch!') : console.log(i); 
+// }
+
+// let greeting = 'Aloha!';
+// for (let i = 0; i < 3; i++) {
+//   console.log(greeting)
+// }
+
+let array = [1, 2, 3, 4];
+let index = 0;
+
+while (index < array.length) {
+
+
+
+  
+  console.log(array[index])
+  index++;
+}
